f3a86fe5aa82070cbd0aebb2b369e44e7dfa5861
Cody Lewis <cody01546@gmail.com>
2018-08-16 09:29:18 -0500

    implement state & update render content

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      trueStatement: 'foo & bar are excellent variable names',
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -9,7 +17,8 @@ export default class App extends React.Component {
           <Text>Lightning Talks are great!</Text>
         </View>
         <View style={[styles.container, styles.darkSide]}>
-          <Text style={{ color: 'white' }}>This is the dark side</Text>
+          <Text style={{ color: 'white', marginBottom: '5%' }}>This is the dark side</Text>
+          <Text style={{ color: 'white' }}>{this.state.trueStatement}</Text>
         </View>
       </View>
     );
