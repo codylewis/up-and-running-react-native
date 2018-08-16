a0f103175b717511b4524bbbb1cc8fe88dd961ea
Cody Lewis <cody01546@gmail.com>
2018-08-16 09:39:47 -0500

    add touchable highlight, the real truth & refactor css

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight } from 'react-native';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -7,13 +7,25 @@ export default class App extends React.Component {
 
     this.state = {
       trueStatement: 'foo & bar are excellent variable names',
+      balanceInTheForce: true,
     }
+
+    this.theRealTruth = this.theRealTruth.bind(this);
+  }
+
+  theRealTruth() {
+    this.setState({
+      trueStatement: 'all varaible names are excellent varliable names',
+      balanceInTheForce: false,
+    });
   }
 
   render() {
+    let additionalStyles = this.state.balanceInTheForce ? {} : styles.displayNone;
+
     return (
       <View style={styles.container}>
-        <View style={styles.container}>
+        <View style={[styles.container, additionalStyles]}>
           <Image
             style={{ height: 200, width: 250 }}
             source={require('./milwaukee-js-logo.jpeg')}
@@ -21,10 +33,12 @@ export default class App extends React.Component {
           />
           <Text>Lightning Talks are great!</Text>
         </View>
-        <View style={[styles.container, styles.darkSide]}>
-          <Text style={{ color: 'white', marginBottom: '5%' }}>This is the dark side</Text>
-          <Text style={{ color: 'white' }}>{this.state.trueStatement}</Text>
-        </View>
+        <TouchableHighlight style={[styles.container, styles.darkSide]} onPress={this.theRealTruth}>
+          <View>
+            <Text style={[styles.darkSideText, { marginBottom: '5%' }]}>This is the dark side</Text>
+            <Text style={styles.darkSideText}>{this.state.trueStatement}</Text>
+          </View>
+        </TouchableHighlight>
       </View>
     );
   }
@@ -40,5 +54,14 @@ const styles = StyleSheet.create({
 
   darkSide: {
     backgroundColor: 'black'
+  },
+
+  darkSideText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+
+  displayNone: {
+    display: 'none',
   }
 });
